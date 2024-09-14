3ae23b9138353f88cd62e0b5a11c7e2caa275114
Yengner <Yengnerb475@gmail.com>
2024-09-14 19:58:11 -0400

    updated package build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
-// app/page.tsx (or any other path in the /app directory)
-import prisma from '../../utils/db'; // Make sure this path is correct
+import { supabase } from "@/lib/supabaseClient";
 
 export default async function Page() {
-  // Fetch data directly in the Server Component
-  const user = await prisma.user.findFirst({
-    where: {
-      email: "test@test.com",
-    },
-  });
+  const { data: users, error } = await supabase.from("users").select("*");
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
 
   // You can now use the user object directly in your component
   return (
     <div>
       <h1>User Info</h1>
-      {user ? (
-        <div>
-          <p>Email: {user.email}</p>
-          <p>Name: {user.name}</p>
-        </div>
-      ) : (
-        <p>No user found</p>
-      )}
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
     </div>
   );
 }
