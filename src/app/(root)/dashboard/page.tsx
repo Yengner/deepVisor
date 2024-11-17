8669acf845096e10e4770925ddaff1e8f4393890
Yengner <Yengnerb475@gmail.com>
2024-11-16 21:15:01 -0500

    AGAIN

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -2,7 +2,8 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 // import ECommerce from "@/components/Dashboard/Marketing";
 import { getFbAdAccount, getFbAdAccounts } from "@/lib/actions/facebook/facebook.actions";
 
-const Dashboard = async({ searchParams: { id }}:SearchParamProps) => {
+const Dashboard = async() => {
+  // put back id YENGNER { searchParams: { id }}:SearchParamProps
   // const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const userId = loggedIn.id;
@@ -10,21 +11,21 @@ const Dashboard = async({ searchParams: { id }}:SearchParamProps) => {
   
   if(!fBAdAccounts) return;
 
-  const fBAdAccountsData = fBAdAccounts?.data;
-  const adAccountId = (id as string) || fBAdAccountsData[0]?.adAccountId;
+  // const fBAdAccountsData = fBAdAccounts?.data;
+  // const adAccountId = (id as string) || fBAdAccountsData[0]?.adAccountId;
 
-  const account = await getFbAdAccount({ adAccountId, userId });
+  // const account = await getFbAdAccount({ adAccountId, userId });
 
-  const safeAccount = {
-    ...account,
-    accountInfo: account.accountInfo || [], // Fallback to empty array if `null`
-  };
+  // const safeAccount = {
+  //   ...account,
+  //   accountInfo: account.accountInfo || [], // Fallback to empty array if `null`
+  // };
   // fBAdAccounts.data.forEach(adAccount => {
   //   console.log(`Ad Account Id: ${adAccount.adAccountId}`);
   //   console.log('Campaigns:', adAccount.campaigns)
   // })
   
-  const adAccountCampaigns = account.campaigns
+  // const adAccountCampaigns = account.campaigns
   return (
     <h1>dashboard</h1>
     // <ECommerce 
