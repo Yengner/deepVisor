95eee4e72f3540f5d24dd9c67ffdc2271780dfd4
Yengner <Yengnerb475@gmail.com>
2025-01-17 19:53:15 -0500

    fixed hydration error

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../../styles/globals.css";
+import { Source_Sans_3, Manrope } from "next/font/google";
 import Header from "@/components/public/Header";
 import Footer from "@/components/public/Footer";
 import Script from "next/script";
@@ -9,6 +10,10 @@ export const metadata: Metadata = {
   description: "Helping businesses grow with advanced ad tools and insights.",
 };
 
+const manrope = Manrope({ subsets: ['latin'] });
+const sourceSans = Source_Sans_3({ subsets: ['latin'] });
+
+
 export default function RootLayout({
   children,
 }: {
@@ -16,26 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager Script */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KPR97KV2');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager Script */}
-      </head>
-      <body>
+      <body className={`${manrope.className} ${sourceSans.className} antialiasedbg-gray-200 relative h-screen`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
@@ -46,6 +32,24 @@ export default function RootLayout({
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KPR97KV2');
+          `,
+          }}
+        />
+        {/* End Google Tag Manager Script */}
 
         <Header />
         <main>{children}</main>
