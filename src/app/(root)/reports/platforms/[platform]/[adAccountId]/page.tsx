e8ee2317e37d26177a89861a660a6ff8214d3add
Yengner <Yengnerb475@gmail.com>
2025-02-05 13:42:52 -0500

    added dynamic user to ad acocunts pages.

--- src/app/(root)/reports/platforms/[platform]/[adAccountId]/page.tsx
+++ src/app/(root)/reports/platforms/[platform]/[adAccountId]/page.tsx
@@ -1,5 +1,7 @@
 import ClientDashboard from '@/components/ClientDashboard';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { fetchDashboardMetrics } from '@/lib/api/dashboard';
+import { getAdAccountsData } from '@/lib/api/getAdAccountsData';
 
 interface AdAccountPageProps {
   params: Promise<{
@@ -13,11 +15,18 @@ export default async function AdAccountPage({
 }: AdAccountPageProps) {
   try {
     const resolvedParams = await params;
-
     const { platform, adAccountId } = resolvedParams;
 
+    const loggedIn = await getLoggedInUser();
+    const userId = loggedIn.id;
+
+    const adAccountData = await getAdAccountsData(platform, userId);
     const dashboardData = await fetchDashboardMetrics(platform, adAccountId);
-    return <ClientDashboard dashboardData={dashboardData} />;
+    return (
+
+      <ClientDashboard dashboardData={dashboardData} />
+
+    )
   } catch (error) {
     console.error('Error resolving params or fetching dashboard data:', error);
     return (<div className="text-red-600">How did you get here?</div>)
